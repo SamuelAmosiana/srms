3770ba2711a6b02e49582c7d54f0d567a281f34e
AmCodeSmith <112726326+SamuelAmosiana@users.noreply.github.com>
2025-11-13 12:16:17 +0200

    Updated email_config.php

--- email_config.php
+++ email_config.php
@@ -6,14 +6,14 @@
  */
 
 // Email configuration constants
-define('SMTP_HOST', 'mail.lsuczm.com');
-define('SMTP_USERNAME', 'admissions@lsuczm.com');
-define('SMTP_PASSWORD', '#@adm1missions');
+define('SMTP_HOST', '');
+define('SMTP_USERNAME', '');
+define('SMTP_PASSWORD', '');
 define('SMTP_PORT', 587);
 define('SMTP_SECURE', 'tls');
-define('EMAIL_FROM', 'admissions@lsuczm.com');
+define('EMAIL_FROM', '');
 define('EMAIL_FROM_NAME', 'LSC Admissions Office');
-define('EMAIL_REPLY_TO', 'admissions@lsuczm.com');
+define('EMAIL_REPLY_TO', '');
 define('PORTAL_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/srms/login.php');
 
 // Test email function
