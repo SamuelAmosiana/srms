90e6573ca95e1fb7a089356c7fd8916e6f6cbd9a
SamuelAmosiana <sianamatesamuel@gmail.com>
2025-12-31 10:12:48 +0200

    Add error output to see why server error after approving student by enrollment officer

--- enrollment/undergraduate_applications.php
+++ enrollment/undergraduate_applications.php
@@ -1,4 +1,9 @@
 <?php
+// Enable real error output
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require '../config.php';
 require '../auth.php';
 
